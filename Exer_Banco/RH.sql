eebb60f1666478842a7681ff210206797c2cd498
Sthefany Sousa <sthefanyssbastos@hotmail.com>
2023-12-04 14:11:37 -0300

    FIX

--- Exer_Banco/RH.sql
+++ Exer_Banco/RH.sql
@@ -6,17 +6,17 @@ CREATE TABLE colaboradores_empresa (
 matricula_id bigint AUTO_INCREMENT,
 nome VARCHAR (255) NOT NULL,
 salario decimal (8,2) NOT NULL,
-horario int,
+horario time,
 dependentes int,
 PRIMARY KEY (matricula_id)
 );
 
 INSERT INTO colaboradores_empresa ( nome, salario, horario, dependentes)
-VALUES ("Fábio Oliveira", 1450, 12 , 2),
-("Samanta Peixoto", 1800, 8 , 1),
-("Henrique Sebastian", 2300, 18 , 3),
-("Manoel De Assis", 1364, 12 , 0),
-("Oliver Oliveira", 1450, 14 , 9);
+VALUES ("Fábio Oliveira", 1450, "12:00" , 2),
+("Samanta Peixoto", 1800, "8:00" , 1),
+("Henrique Sebastian", 2300, "18:00" , 3),
+("Manoel De Assis", 1364, "12:00" , 0),
+("Oliver Oliveira", 1450, "14:00" , 9);
 
 SELECT * FROM colaboradores_empresa;
 
@@ -24,4 +24,9 @@ SELECT * FROM colaboradores_empresa WHERE salario > 2000;
 
 SELECT * FROM colaboradores_empresa WHERE salario < 2000;
 
+SELECT HORARIO, TIME_FORMAT (horario, '%H:%i') AS horário
+FROM colaboradores_empresa;
+
 UPDATE colaboradores_empresa SET dependentes = 2 WHERE matricula_id = 5;
+
+DROP TABLE colaboradores_empresa;
